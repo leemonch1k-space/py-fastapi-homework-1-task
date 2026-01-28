b042a9400623b28aef448e7d812c6671cdd0e59c
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-01-28 15:42:26 +0200

    updated according to review

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,7 +1,7 @@
 import math
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -13,35 +13,13 @@ router = APIRouter()
 
 PAGE_BASE_URL = "/theater/movies/"
 
+
 @router.get("/movies/", response_model=MovieListResponseSchema)
 async def get_movies(
-        db: Annotated[AsyncSession,
-        Depends(get_db)],
-        page: int,
-        per_page: int
+        db: Annotated[AsyncSession, Depends(get_db)],
+        page: int = Query(default=1, ge=1),
+        per_page: int = Query(default=10, ge=1, le=20)
 ):
-    # Handling query exceptions
-    if page <= 0 or per_page <= 0:
-        invalid_query = None
-
-        if page <= 0 < per_page:
-            invalid_query = "page"
-        elif per_page <= 0 < page:
-            invalid_query = "per_page"
-        else:
-            invalid_query = ["page", "per_page"]
-
-        raise HTTPException(
-            status_code=422,
-            detail=[
-                {
-                    "loc": ["query", invalid_query],
-                    "msg": "ensure this value is greater than or equal to 1",
-                    "type": "value_error.number.not_ge"
-                }
-            ]
-        )
-
     # Count total movies
     count_query = select(func.count()).select_from(MovieModel)
     total_result = await db.execute(count_query)
@@ -76,6 +54,7 @@ async def get_movies(
         "total_pages": total_pages,
         "total_items": total_items
     }
+
 
 @router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
 async def get_movie(movie_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
